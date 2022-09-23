b3c272822355fcd05f01463c21d2c40d33c5354c
NilKad <kadulin@gmail.com>
2022-09-24 02:59:59 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionClass: 'gallery__captionClass',
+});
+
+const createGalletyMarkup = gallery =>
+  gallery
+    .map(
+      ({ original, preview, description }) =>
+        `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`
+    )
+    .join('');
+
+galleryRef.innerHTML = createGalletyMarkup(galleryItems);
+
+lightbox.refresh();
 // Change code below this line
 
 console.log(galleryItems);
